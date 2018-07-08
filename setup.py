824e4f95d13f9d8ee16cea52bda54e91aed93436
Cyxapic <truecyxapic@yandex.ru>
2018-07-08 16:14:36 +0300

    alfa 0.82

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-secrecy",
-    version='0.81.4',
+    version='0.82',
     include_package_data=True,
     description="Django secret project variables",
     long_description="Django app which generates JSON file with 'secret project variables'",
