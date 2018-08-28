26562276bf7c6ad6b862522703e6df4c51187abb
Cyxapic <truecyxapic@yandex.ru>
2018-08-28 20:51:24 +0300

    HOTFIX secrecy.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 
 setup(
     name="django-secrecy",
-    version='0.99',
+    version='0.99.1',
     include_package_data=True,
     description="Django secret project variables",
     long_description=long_description,
